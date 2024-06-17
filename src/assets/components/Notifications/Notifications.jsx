e01b3408c3804f6d5a480f172c9e798f296c6d34
Indica Fathsyah <zachajasz@gmail.com>
2024-06-17 10:06:28 +0700

    Add Not Completed data notification feature

--- src/assets/components/Notifications/Notifications.jsx
+++ src/assets/components/Notifications/Notifications.jsx
@@ -6,6 +6,7 @@ import "./Notifications.scss";
 
 function Notifications() {
   const [expiredUsers, setExpiredUsers] = useState([]);
+  const [incompleteUsers, setIncompleteUsers] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -18,7 +19,11 @@ function Notifications() {
       const expiredUsers = userData.filter(
         (user) => new Date(user.expiDate) < currentDate
       );
+      const incompleteUsers = userData.filter(
+        (user) => !user.expiDate || !user.companyName
+      );
       setExpiredUsers(expiredUsers);
+      setIncompleteUsers(incompleteUsers);
     };
 
     fetchUsers();
@@ -27,33 +32,48 @@ function Notifications() {
   return (
     <div className="notifications">
       <div className="notif-wrapper">
-        <div className="expired-users-list">
+        <div className="expired-wrapper">
           <h2>Expired Licenses</h2>
-          {expiredUsers.map((user) => (
-            <div key={user.id} className="expired-user-item">
-              <div className="notif-icon">
-                <RiPassExpiredFill />
+          <div className="expired-users-list">
+            {expiredUsers.map((user) => (
+              <div key={user.id} className="expired-user-item">
+                <div className="notif-icon">
+                  <RiPassExpiredFill />
+                </div>
+                <div className="notif-info">
+                  <h3>
+                    {user.name} ({user.companyName})
+                  </h3>
+                  <p>Simper License expired on {user.expiDate}</p>
+                </div>
               </div>
-              <div className="notif-info">
-                <h3>{user.name} ({user.companyName})</h3>
-                <p>Simper License expired on {user.expiDate}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-        <div className="not-filled-user-list">
+        <div className="unfilled-wrapper">
           <h2>Uncompleted Data</h2>
-          {expiredUsers.map((user) => (
-            <div key={user.id} className="expired-user-item">
-              <div className="notif-icon">
-                <RiContactsBook2Line />
+          <div className="not-filled-user-list">
+            {incompleteUsers.map((user) => (
+              <div key={user.id} className="not-filled-user-item">
+                <div className="notif-icon">
+                  <RiContactsBook2Line />
+                </div>
+                <div className="notif-info">
+                  <h3>{user.name}</h3>
+                  <p>
+                    {user.companyName
+                      ? user.companyName
+                      : "Company name not provided"}
+                  </p>
+                  <p>
+                    {user.expiDate
+                      ? `Simper License expired on ${user.expiDate}`
+                      : "Expiration date not provided"}
+                  </p>
+                </div>
               </div>
-              <div className="notif-info">
-                <h3>{user.name} {user.companyName}</h3>
-                <p>Simper License expired on {user.expiDate}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
